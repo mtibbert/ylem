cfea3f3cc19c8a0c76ca294fb97d04119c9c91cb
mtibbert <mtibbert@users.noreply.github.com>
2018-11-05 08:00:54 -0600

    Moved type utitlies tests into tests folder

--- temporencUtils/types/tests/testsuite.py
+++ temporencUtils/types/tests/testsuite.py
@@ -3,11 +3,14 @@ import unittest
 from temporencUtils.types.tests.base_type_test import BaseTypeTest
 from temporencUtils.types.tests.type_d_test import TypeDTest
 from temporencUtils.types.tests.type_t_test import TypeTTest
+from temporencUtils.types.tests.type_utils_test import TypeUtilsTest
 
 verbosity = 2  # 0 is quiet; 2 lists tests as run
 
 
 if __name__ == "__main__":    # Make individual suites for test cases
+    type_utils_test_suite = unittest.TestLoader() \
+        .loadTestsFromTestCase(TypeUtilsTest)
     base_type_test_suite = unittest.TestLoader() \
         .loadTestsFromTestCase(BaseTypeTest)
     type_d_test_suite = unittest.TestLoader() \
@@ -19,7 +22,7 @@ if __name__ == "__main__":    # Make individual suites for test cases
     suites = [base_type_test_suite,
               type_d_test_suite,
               type_t_test_suite,
-              ]
+              type_utils_test_suite,]
 
     # run the suites
     suite = unittest.TestSuite(suites)
